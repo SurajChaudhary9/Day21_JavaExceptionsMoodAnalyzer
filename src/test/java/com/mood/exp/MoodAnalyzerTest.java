5316dba7840c9a644f7ea6e4f15d37a45c90d33e
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-03-23 16:31:25 +0530

    Added : Happy or Sad mood check

---
+++ src/test/java/com/mood/exp/MoodAnalyzerTest.java
@@ -0,0 +1,18 @@
+package com.mood.exp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MoodAnalyzerTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
